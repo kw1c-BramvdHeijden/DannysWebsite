424455f415e3ec4a4b007a139453063cb636947b
kw1c-HorstmanKes <kes.horstman@edu-kw1c.nl>
2026-05-12 09:03:25 +0200

    formatting alles

--- spelregels.php
+++ spelregels.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Boules Competities | Spelregels</title>
-  <link rel="stylesheet" href="spelregels.css">
+  <link rel="stylesheet" href="css/spelregels.css">
 </head>
 <body>
   <div class="page-shell">
