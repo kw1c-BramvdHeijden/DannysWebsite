4fa1faccdcad707926944ad95e482b1b1e8cb5ab
cohor <connor.horstman@edu-kw1c.nl>
2026-05-19 13:57:02 +0200

    made includes folder,header footer files.

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<?php
+
+//<header class="site-header">
+  //          <a class="brand" href="#home" data-i18n-aria-label="nav.brandHome" aria-label="Boules Competities home">
+    //            <span class="brand-ball" aria-hidden="true"></span>
+      //          <span class="brand-copy">
+        //            <strong>BOULES</strong>
+          //          <small data-i18n="brand.subtitle">COMPETITIES</small>
+            //    </span>
+            //</a>
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+include_once("includes/header.php");
+include_once("includes/footer.php");
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
