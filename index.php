9a8e2f8314b6dcf626830fbab1799cd62efa12ab
kw1c-HorstmanKes <kes.horstman@edu-kw1c.nl>
2026-06-16 12:32:39 +0200

    header.css, footer.css, index heeft nu include footer, footer zit nu in footer.php

--- index.php
+++ index.php
@@ -154,13 +154,7 @@ include_once("includes/leaderboard.php");
         </section>
     </main>
 
-    <footer class="quote-footer" id="over">
-        <div class="quote-inner">
-            <div class="footer-boules" aria-hidden="true"><span></span><span></span><span class="small-red"></span></div>
-            <blockquote data-i18n-html="footer.quote">" Jeu de boules is meer dan een spel.<br>Het is samenzijn, strategie en plezier. "</blockquote>
-            <div class="footer-leaf" aria-hidden="true"></div>
-        </div>
-    </footer>
+    <?php include 'includes/footer.php';?>
 
     <!-- Auth modal is rendered by includes/header.php. -->
     <div class="competition-modal" data-competition-modal hidden>
