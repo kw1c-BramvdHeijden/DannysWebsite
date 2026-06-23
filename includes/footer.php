a32bfd048c44a174bcc1c11f5b0df6e48273d2fd
cohor <connor.horstman@edu-kw1c.nl>
2026-06-23 12:40:45 +0200

    ai slogan vervangen

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="quote-footer" id="over">
         <div class="quote-inner">
             <div class="footer-boules" aria-hidden="true"><span></span><span></span><span class="small-red"></span></div>
-            <blockquote data-i18n-html="footer.quote">" Jeu de boules is meer dan een spel.<br>Het is samenzijn, strategie en plezier. "</blockquote>
+            <blockquote data-i18n-html="footer.quote">" Laat de ballen ketsen<br>En raak aan het kletsen! "</blockquote>
         </div>
     </footer>
